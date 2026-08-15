6750c28f8d7f2b2fe906597ee4e5948a51618773
asgofficial <gautamayushsinghofficial@gmail.com>
2026-08-15 14:57:43 +0530

    Delete src directory

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import ComingSoonPage, { AdminPanel } from './frame2.jsx'
-
-function App() {
-  return window.location.pathname.startsWith('/admin') ? <AdminPanel /> : <ComingSoonPage />
-}
-
-export default App
